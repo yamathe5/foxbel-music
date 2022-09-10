cc57a99bb10d7faab283346832d62dea019ea464
yamathe <manuelsegmar@gmail.com>
2022-09-10 16:16:10 -0500

    ADD: music player buttons component

---
+++ src/components/PlayMusicButtons/PlayMusicButtons.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./play-music-buttons.scss";
+
+export default function PlayMusicButtons() {
+  return (
+    <div className="music-player-bar">
+      <div className="music-player-bar__info">Cancioin</div>
+      <div className="music-player-bar__play-buttons">bttons</div>
+      <div className="music-player-bar__volumen-controls">volumen</div>
+    </div>
+  );
+}
